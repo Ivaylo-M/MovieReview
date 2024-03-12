fd1ac252f915653f6bb7677b97b7a25574ae172e
ivaylo-milanov <imilanov24@gmail.com>
2024-03-12 18:26:24 +0200

    Added repository pattern to the project and added the repository to the dependency injection

---
+++ API/Infrastructure/AddServicesExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace API.Infrastructure
+{
+    using Application.Users;
+    using Persistence.Repositories;
+
+    public static class AddServicesExtension
+    {
+        public static IServiceCollection ConfigurateServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, Repository>();
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(LoginUser).Assembly));
+
+            return services;
+        }
+    }
+}
