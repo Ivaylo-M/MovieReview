916eafb81df576ea44e12f562e479c3769c6a4e2
ivaylo-milanov <imilanov24@gmail.com>
2024-03-12 19:09:37 +0200

    Added User controller

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿namespace API.Controllers
+{
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public UserController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [Route("register")]
+        [HttpPost]
+        public ActionResult Register()
+        {
+            return Ok(true);
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public ActionResult Login()
+        {
+            return Ok(true);
+        }
+
+        //[Authorize]
+        [Route("logout")]
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            return Ok(true);
+        }
+    }
+}
