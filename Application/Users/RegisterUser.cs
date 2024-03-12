51fdd3721057dcd10994ff6e456a5898d009f8c0
ivaylo-milanov <imilanov24@gmail.com>
2024-03-12 19:44:39 +0200

    Added register tests setup

---
+++ Application/Users/RegisterUser.cs
@@ -0,0 +1,39 @@
+﻿namespace Application.Users
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    
+    using MediatR;
+    using Microsoft.AspNetCore.Identity;
+    
+    using Domain;
+    using Application.Response;
+    using Application.Services.Contracts;
+
+    public class RegisterUser
+    {
+        public class RegisterUserCommand : IRequest<Result<Unit>> { }
+
+        public class RegisterUserHandler : IRequestHandler<RegisterUserCommand, Result<Unit>>
+        {
+            private readonly UserManager<User> userManager;
+            private readonly SignInManager<User> signInManager;
+            private readonly ITokenService tokenService;
+
+            public RegisterUserHandler(
+                UserManager<User> userManager,
+                SignInManager<User> signInManager,
+                ITokenService tokenService)
+            {
+                this.userManager = userManager;
+                this.signInManager = signInManager;
+                this.tokenService = tokenService;
+            }
+
+            public async Task<Result<Unit>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+            {
+                return Result<Unit>.Success(Unit.Value);
+            }
+        }
+    }
+}
--- Tests/User/RegisterUserTests.cs
+++ Tests/User/RegisterUserTests.cs
@@ -1,12 +1,51 @@
 ﻿namespace Tests.User
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using Moq;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+    
+    using Domain;
+    using Application.Services.Contracts;
+    
+    using static Application.Users.RegisterUser;
 
-    internal class RegisterUserTests
+    [TestFixture]
+    public class RegisterUserTests
     {
+        private Mock<ITokenService> tokenServiceMock;
+        private Mock<UserManager<User>> userManagerMock;
+        private Mock<SignInManager<User>> userSignInManagerMock;
+        private RegisterUserHandler handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.tokenServiceMock = new Mock<ITokenService>();
+
+            this.userManagerMock = new Mock<UserManager<User>>(
+                new Mock<IUserStore<User>>().Object,
+                new Mock<IOptions<IdentityOptions>>().Object,
+                new Mock<IPasswordHasher<User>>().Object,
+                new IUserValidator<User>[0],
+                new IPasswordValidator<User>[0],
+                new Mock<ILookupNormalizer>().Object,
+                new Mock<IdentityErrorDescriber>().Object,
+                new Mock<IServiceProvider>().Object,
+                new Mock<ILogger<UserManager<User>>>().Object
+            );
+
+            this.userSignInManagerMock = new Mock<SignInManager<User>>(
+                userManagerMock.Object,
+                Mock.Of<IHttpContextAccessor>(),
+                Mock.Of<IUserClaimsPrincipalFactory<User>>(),
+                null!, null!, null!, null!);
+
+            handler = new RegisterUserHandler(
+                this.userManagerMock.Object,
+                this.userSignInManagerMock.Object, 
+                this.tokenServiceMock.Object);
+        }
     }
 }
